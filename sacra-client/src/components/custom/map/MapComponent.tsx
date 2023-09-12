cf22bf524b4781b3eb9199b40ecf9552b6120ff9
vi <vitalya.permyakov155@gmail.com>
2023-09-12 10:42:28 +0700

    feat(client): MarkerIcon

--- sacra-client/src/components/custom/map/MapComponent.tsx
+++ sacra-client/src/components/custom/map/MapComponent.tsx
@@ -5,14 +5,13 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 import type { ObjectsType } from '@/lib/schemas/strapi-schemas';
 import React from 'react'
 import Map, { Marker } from 'react-map-gl'
+import MarkerIcon from './MarkerIcon';
 
 export default function MapComponent({
     objects,
 }: {
     objects: ObjectsType
 }) {
-    
-    objects.data.map(el => console.log(el.attributes.geolocation))
 
   return (
     <div className='w-full'>
@@ -24,16 +23,18 @@ export default function MapComponent({
               zoom: 14
             }}
             style={{width: "100%",  margin: "0 auto", height: "80vh"}}
-            mapStyle="mapbox://styles/mapbox/streets-v9"
+            mapStyle="mapbox://styles/mapbox/navigation-night-v1"
         >
             {objects.data.map((el, index) => (
                 <Marker 
                     key={index}
                     longitude={el.attributes.geolocation.longitude}
                     latitude={el.attributes.geolocation.latitude}
-                    anchor="bottom"
+                    anchor="center"
                 >
-                  <h1 className=' text-4xl text-red-700'>Marker</h1>
+                  <div className=' cursor-pointer'>
+                    <MarkerIcon />
+                  </div>
                 </Marker>
             ))}
         </Map>
